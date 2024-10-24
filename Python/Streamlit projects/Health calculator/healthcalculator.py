e7b6ca8c52fff15118e7c72fba5ebed2490fb092
Swayam-Swapnila-Das7 <swayamswapnila.code@gmail.com>
2024-10-24 11:19:32 +0530

    Update healthcalculator.py

--- Python/Streamlit projects/Health calculator/healthcalculator.py
+++ Python/Streamlit projects/Health calculator/healthcalculator.py
@@ -46,16 +46,16 @@ if mode == "BMI":
         bmi = calculate_bmi(weight, height)
         st.write(f"Your BMI is: {bmi:.2f}")
         if bmi < 18.5:
-            st.markdown("<p style='color:red;'>Underweight</p>", unsafe_allow_html=True)
+            st.warning(" You are Underweight, try to gain weight in a healthy style.")
             suggestion = "Consider increasing your caloric intake with nutrient-dense foods."
         elif 18.5 <= bmi < 24.9:
-            st.markdown("<p style='color:green;'>Normal weight</p>", unsafe_allow_html=True)
+            st.success("Normal weight. You are in a good condition maintain it.")
             suggestion = "Great job! Maintain a balanced diet and regular exercise."
         elif 25 <= bmi < 29.9:
-            st.markdown("<p style='color:red;'>Overweight</p>", unsafe_allow_html=True)
+            st.warning(" You are Overweight ")
             suggestion = "Consider adopting a healthier lifestyle with regular physical activity."
         else:
-            st.markdown("<p style='color:red;'>Obesity</p>", unsafe_allow_html=True)
+            st.error("Obesity")
             suggestion = "Consult a healthcare provider for personalized advice."
 
 elif mode == "BMR":
@@ -93,10 +93,10 @@ elif mode == "Body Fat Percentage":
         body_fat = calculate_body_fat(age, waist_circumference, neck_circumference, hip_circumference, height, gender)
         st.write(f"Your Body Fat Percentage is: {body_fat:.2f}%")
         if body_fat < 18:
-            st.markdown("<p style='color:green;'>Healthy range</p>", unsafe_allow_html=True)
+            st.success("You are in a Healthy range ,maintain it 💪")
             suggestion = "Keep up the good work!"
         else:
-            st.markdown("<p style='color:red;'>Consult a health professional</p>", unsafe_allow_html=True)
+            st.warning("Consult a health professional , You have mor fat.")
             suggestion = "Consider discussing your body composition with a healthcare provider."
 
 # Suggestions and Resources
